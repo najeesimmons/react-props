2e162858ca259b2984a46f6cbbcfc2098a019044
najeesimmons <najee.bryant.simmons@gmail.com>
2022-04-19 21:50:31 -0400

    adding comments for clarity

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+// This statement makes react available in this file for 
+// babel to translate jsx and have access to React.createElement()
 import React from 'react'
 
 function Hello() {
+  // Every component has, at minimum, a return. It generates a Virtual 
+  // DOM node that will be added to the actual DOM.
   return (
     <h1>Hello World</h1>
-  )
+  );
 }
 
-export default App;
+// This exposes the Hello class to other files which import from the App.js 
+// file. 
+export default Hello;
